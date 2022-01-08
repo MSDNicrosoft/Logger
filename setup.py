107c5d009d0513a4e0a0459e22e528171d401226
MSDNicrosoft <wang3311835119@hotmail.com>
2022-01-08 16:59:00 +0800

    统一符号

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import find_packages, setup
 NAME = "msdnicrosoft_logger"
 DESCRIPTION = "自己手写的 logger"
 URL = "https://github.com/MSDNicrosoft/logger"
-EMAIL = 'wang3311835119@hotmail.com'
-AUTHOR = 'MSDNicrosoft'
-REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.1'
-REQUIRED = ['colorama>=0.4.4']
+EMAIL = "wang3311835119@hotmail.com"
+AUTHOR = "MSDNicrosoft"
+REQUIRES_PYTHON = ">=3.6.0"
+VERSION = "0.0.1"
+REQUIRED = ["colorama>=0.4.4"]
 
 try:
-    with open('README.md','r',encoding='utf-8') as desc:
+    with open("README.md","r",encoding="utf-8") as desc:
         long_description = desc.read()
 except FileNotFoundError:
     long_description = DESCRIPTION
